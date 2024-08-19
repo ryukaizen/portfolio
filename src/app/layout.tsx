abf8545b037c2a54af6a49c537fdc4077c7836a1
ryukaizen <akashajaydalvi@gmail.com>
2024-08-19 23:30:24 +0530

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import Sidebar from '../components/Sidebar';
+import Head from 'next/head';
 import { Tilt_Warp } from "next/font/google";
 import { FaLinkedin, FaGithub, FaTelegram } from 'react-icons/fa';
 
@@ -17,19 +18,21 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
             <body className={`${font.className}`}>
-            <div className="absolute top-4 right-4 flex space-x-4">
-        <a href="https://github.com/ryukaizen" target="_blank" aria-label="GitHub">
-          <FaGithub className="h-12 w-12 text-gray-900 hover:text-gray-700" />
-        </a>
-        <a href="https://linkedin.com/in/" target="_blank" aria-label="LinkedIn">
-          <FaLinkedin className="h-12 w-12 text-blue-700 hover:text-blue-600" />
-        </a>
-        <a href="https://telegram.me/ryukaizen/" target="_blank" aria-label="Telegram">
-        <FaTelegram className="h-12 w-12 text-[#0088cc] hover:text-[#0088cc99]" />
-        </a>
-  
-        </div>
+                <div className="absolute top-4 right-4 flex space-x-4">
+                    <a href="https://github.com/ryukaizen" target="_blank" aria-label="GitHub">
+                        <FaGithub className="h-12 w-12 text-gray-900 hover:text-gray-700" />
+                    </a>
+                    <a href="https://linkedin.com/in/" target="_blank" aria-label="LinkedIn">
+                        <FaLinkedin className="h-12 w-12 text-blue-700 hover:text-blue-600" />
+                    </a>
+                    <a href="https://telegram.me/ryukaizen/" target="_blank" aria-label="Telegram">
+                        <FaTelegram className="h-12 w-12 text-[#0088cc] hover:text-[#0088cc99]" />
+                    </a>
+                </div>
                 <div style={{ display: 'flex' }}>
                 <div className="fixed inset-0 z-[-1] overflow-hidden">
                     <div className="glow-circle glow-circle-1"></div>
