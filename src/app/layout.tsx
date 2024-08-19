8a22d5dc95dc3a28e66de1c49061fa0ab1bbb3d0
ryukaizen <akashajaydalvi@gmail.com>
2024-08-19 21:30:50 +0530

    feat: add social links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Sidebar from '../components/Sidebar';
 import { Tilt_Warp } from "next/font/google";
+import { FaLinkedin, FaGithub, FaTelegram } from 'react-icons/fa';
 
 const font = Tilt_Warp({ weight: "400", subsets: ["latin-ext"] });
 
@@ -17,6 +18,18 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${font.className}`}>
+            <div className="absolute top-4 right-4 flex space-x-4">
+        <a href="https://github.com/ryukaizen" target="_blank" aria-label="GitHub">
+          <FaGithub className="h-12 w-12 text-gray-900 hover:text-gray-700" />
+        </a>
+        <a href="https://linkedin.com/in/" target="_blank" aria-label="LinkedIn">
+          <FaLinkedin className="h-12 w-12 text-blue-700 hover:text-blue-600" />
+        </a>
+        <a href="https://telegram.me/ryukaizen/" target="_blank" aria-label="Telegram">
+        <FaTelegram className="h-12 w-12 text-[#0088cc] hover:text-[#0088cc99]" />
+        </a>
+  
+        </div>
                 <div style={{ display: 'flex' }}>
                 <div className="fixed inset-0 z-[-1] overflow-hidden">
                     <div className="glow-circle glow-circle-1"></div>
