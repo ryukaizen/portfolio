f01b2255da7666e98644366a34bda9a49876619e
ryukaizen <akashajaydalvi@gmail.com>
2024-08-19 19:38:12 +0530

    feat: add image, name/headline on homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+nextConfig.images = {
+    domains: ['i.imgur.com'],
+    };
+
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1 style={{ fontSize: '8rem' }}>Somethings gotta give</h1>
+      <h3 className="header-mini">
+        Hey there, <span className="header-italic">you've stumbled upon the portfolio of...</span>
+      </h3>
+
+      <span className="header-name"> <Image
+        src="https://i.imgur.com/nrUuS3F.jpeg"
+        alt="Akash Dalvi"
+        width={170}
+        height={170}
+        className="rounded-full"
+      /> <h1>Akash Dalvi</h1></span>
 
       <div className="container mx-auto h-full">
 				<div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
