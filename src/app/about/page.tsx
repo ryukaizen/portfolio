3bcc7a10f1ef21f12f67bbeec46b3fcdb2d65ced
ryukaizen <akashajaydalvi@gmail.com>
2024-08-19 23:31:13 +0530

    feat: initial commit for four sections

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,16 @@
 const About = () => {
-    return <div>About Content Goes Here</div>;
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+          <h3 className="page-title">
+            About Me
+          </h3>
+    
+          <div className="container mx-auto h-full">
+                    <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+                    </div>
+                </div>
+        </main>
+      );
 };
 
 export default About;
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,16 @@
 const Resume = () => {
-    return <div>Resume Content Goes Here</div>;
+    return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+        <h3 className="page-title">
+            Here&apos;s My Resume
+        </h3>
+
+        <div className="container mx-auto h-full">
+              <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+              </div>
+          </div>
+    </main>
+  );
 };
 
 export default Resume;
